d8f2740ccd99e40717084a52f140a936510a1b4b
9Gu-Mo <129805449+9Gu-Mo@users.noreply.github.com>
2025-12-04 16:20:30 +0900

    테스트

--- src/components/common/SliderFiveStep.tsx
+++ src/components/common/SliderFiveStep.tsx
@@ -12,20 +12,31 @@ export default function SliderFiveStep() {
   };
 
   const [sliderValue, setSliderValue] = useState<number>(0);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
 
   return (
     <>
       <div className="slider-wrap">
         <Typography>{marksFiveStep[sliderValue] as string}</Typography>
-        <Slider
-          marks={marksFiveStep}
-          step={null}
-          onChange={(value) => setSliderValue(value)}
-          tooltip={{
-            formatter: (value) => marksFiveStep[value as number] as string,
-            rootClassName: "ant-tooltip-custom",
-          }}
-        />
+        <div
+          onTouchStart={() => setTooltipOpen(true)}
+          onTouchEnd={() => setTooltipOpen(false)}
+          onMouseDown={() => setTooltipOpen(true)}
+          onMouseUp={() => setTooltipOpen(false)}
+          className="test"
+        >
+          <Slider
+            marks={marksFiveStep}
+            step={null}
+            value={sliderValue}
+            onChange={(value) => setSliderValue(value)}
+            tooltip={{
+              open: tooltipOpen,
+              formatter: (value) => marksFiveStep[value as number] as string,
+              rootClassName: "ant-tooltip-custom",
+            }}
+          />
+        </div>
       </div>
     </>
   );
